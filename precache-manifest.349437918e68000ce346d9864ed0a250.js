67e482932e5a736b1b2eae5cedc59dc6c35b1589
Yue Xiong <xiongyueee@outlook.com>
2022-02-14 19:51:49 +0800

    Updates

---
+++ precache-manifest.349437918e68000ce346d9864ed0a250.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8929100b6c38b2c64d76fca154d550",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "3ae8fa4a258458d9007b",
+    "url": "/keeper/static/js/2.3e23916d.chunk.js"
+  },
+  {
+    "revision": "00ce8f3b3d7ae3f033cf",
+    "url": "/keeper/static/js/main.fbce8cbe.chunk.js"
+  },
+  {
+    "revision": "af4a55ea2c5d08d39996",
+    "url": "/keeper/static/js/runtime-main.f5c12d9a.js"
+  }
+]);
